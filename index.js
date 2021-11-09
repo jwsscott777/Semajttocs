799e6201c00423c3e97f9d9dd676ebf5c2198461
James Scott <tlmsols50@gmail.com>
2021-11-09 18:44:40 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+console.log(`
+Semajttocs
+
+A developer and creator of all things Javascript and Swift. 
+I especially like creating apps in Vue for cross-platform 
+work and Swift for my Apple devices. 
+
+📔 GITHUB: https://github.com/jwsscott777/Semajttocs
+
+`);
