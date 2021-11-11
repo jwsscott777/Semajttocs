fff8c4c42cb39394600339929c1e3fefa8621ba2
JWSScott777 <jwsscott777@JamesMac17.local>
2021-11-11 16:10:40 -0500

    Added Info

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 const welcome = require('cli-welcome');
 const pkgJSON = require('./package.json');
+const alert = require('reuse-alerts');
 const chalk = require('chalk');
 const log = console.log;
 
 welcome({
 title: `Semajttocs`,
-tagLine: `Welcome from Semajttocs`,
+tagLine: ` Semajttocs is glad your here`,
 description: pkgJSON.description,
 bgColor: `	#6937ff`,
     color: `#000000`,
@@ -17,15 +18,14 @@ version: pkgJSON.version
 
 
 
-log(`${chalk.italic(`A developer and creator of all things Javascript and Swift. 
-I especially like creating apps in Vue for cross-platform 
+log(`${chalk.italic(`A developer and content creator. I prefer Javascript and Swift. 
+I especially like creating apps in Node.js and Vue for cross-platform 
 work and Swift for my Apple devices.` )}
 
  ${chalk.hex(`#6cc644`).inverse(` GITHUB `)} ${chalk.greenBright(`https://github.com/jwsscott777/Semajttocs`)}
+ 
  ${chalk.hex(`#1da1f2`).inverse(` TWITTER `)} ${chalk.greenBright(`https://twitter.com/@Semajttocs777`)}
 
 `);
 
-log(`
-
-`)
+alert({type: 'info', msg: `You can also reach me at: tlmsols50@gmail.com`});
